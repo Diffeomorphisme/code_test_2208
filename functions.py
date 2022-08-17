eb852100a715de9d3b726a8c7aef94417a734a7c
Diffeomorphisme <nicosfaction@msn.com>
2022-08-17 15:15:33 +0200

    Finished price calculations
    Estimates total days for each service, then removes the free days, the days outside of the time scope, and calculate the price based on discounted days and regular days.

--- functions.py
+++ functions.py
@@ -21,8 +21,10 @@ def fetch_customer_data(customer):
 				for element in line:
 					if element.isdigit():
 						customer_data.append(int(element))
-					elif element.replace('.','',1).isdigit() and element.count('.') < 2:
+					elif element.replace('.', '', 1).isdigit() and element.count('.') < 2:
 						customer_data.append(float(element))
+					elif element == "None":
+						customer_data.append(None)
 					else:
 						customer_data.append(element)
 
@@ -31,7 +33,7 @@ def fetch_customer_data(customer):
 			results[field] = customer_data[index]
 	else:
 		for index, field in enumerate(data_fields):
-			results[field] = "None"
+			results[field] = None
 	print(results)
 	return results
 
@@ -45,3 +47,42 @@ def calculate_days_difference(start_date: str, end_date: str, businessdays=False
 	else:
 		difference = numpy.busday_count(begindates=start_date, enddates=end_date)
 	return difference
+
+
+def calculate_free_days(services, start_date: str, end_date: str, free_days: int):
+
+	number_of_days = calculate_days_difference(start_date, end_date, businessdays=False)
+	free_days_old = []
+	for service in services:
+		free_days_old.append(0)
+	test_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+
+	for i in range(number_of_days):
+		sum = 0
+		for service in services:
+			if datetime.datetime.strptime(service.start_date, "%Y-%m-%d") < test_date:
+				service.total_free_days = calculate_days_difference(max(start_date, service.start_date),
+																	test_date.strftime('%Y-%m-%d'),
+																	businessdays=service.workday)
+				sum += service.total_free_days
+
+		if sum < free_days:
+			pass
+		elif sum == free_days:
+			break
+		else:
+			overshoot = sum - free_days
+			increased_days = []
+			for index, service in enumerate(services):
+				if service.total_free_days > free_days_old[index]:
+					increased_days.append(service)
+
+			for i in range(overshoot):
+				print(i)
+				increased_days[-1 - i].total_free_days -= 1
+			break
+
+		for index, service in enumerate(services):
+			free_days_old[index] = service.total_free_days
+		test_date += datetime.timedelta(days=1)
+		print(test_date)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import functions
 
 
 class Customer():
-	# Attributes from DB:
+	# ***Attributes from DB***
 	# 'customerid',
 	# 'price_A', 'price_B', 'price_C',
 	# 'start_A', 'start_B', 'start_C',
@@ -12,8 +12,9 @@ class Customer():
 	# 'discount_A_start', 'discount_B_start', 'discount_C_start',
 	# 'discount_A_end', 'discount_B_end', 'discount_C_end',
 	# 'free_days'
-	# Other attributes:
+	# ***Other attributes***
 	# start_date, end_date
+	# service_a, service_b, service_c
 
 	def __init__(self, customer_data_from_db: dict, start_date: str, end_date: str):
 		for key, value in customer_data_from_db.items():
@@ -28,81 +29,145 @@ class Customer():
 		self.service_c = Service(self.price_C, self.start_C, self.end_date,
 								 self.discount_C_percent, self.discount_C_start, self.discount_C_end, False)
 
+	def _calculate_free_days_end(self, services):
 
-	def _calculate_free_days(self, services):
-		test_date = datetime.datetime.strptime(self.start_date, "%Y-%m-%d")
-		number_of_days = functions.calculate_days_difference(self.start_date, self.end_date, businessdays=False)
+		free_days_old = []
+		earliest_date = self.start_date
+		for service in services:
+			free_days_old.append(0)
+			earliest_date = min(earliest_date, service.start_date)
+		test_date = datetime.datetime.strptime(earliest_date, "%Y-%m-%d")
+		number_of_days = functions.calculate_days_difference(test_date.strftime('%Y-%m-%d'),
+															 self.end_date, businessdays=False)
+
 		for i in range(number_of_days):
 			sum = 0
+			print(f"test_date: {test_date}")
 			for service in services:
-				if datetime.datetime.strptime(service.start_date, "%Y-%m-%d") < test_date:
-					service.free_days = functions.calculate_days_difference(max(self.start_date, service.start_date),
-																			test_date.strftime('%Y-%m-%d'),
-																			businessdays=service.workday)
-					sum += service.free_days
-			if sum == self.free_days:
+				if datetime.datetime.strptime(service.start_date, "%Y-%m-%d") <= test_date:
+					service.total_free_days = functions.calculate_days_difference(service.start_date,
+																				  test_date.strftime('%Y-%m-%d'),
+																				  businessdays=service.workday)
+					sum += service.total_free_days
+			print(f"sum: {sum}")
+			increased_days = []
+			for index, service in enumerate(services):
+				if service.total_free_days > free_days_old[index]:
+					increased_days.append(service)
+
+			if sum < self.free_days:
+				print("Blih")
+				for increased_day in increased_days:
+					increased_day.end_free_days_date = (test_date + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+
+			elif sum == self.free_days:
+				print("Blah")
 				break
-			elif sum == self.free_days + 1:
-				self.service_c.free_days -= 1
-			elif sum == self.free_days + 2:
-				self.service_b.free_days -= 1
-				self.service_c.free_days -= 1
+			else:
+				print("Blouh")
+				overshoot = sum - self.free_days
+				print(f"overshoot: {overshoot}")
+				for i in range(overshoot):
+					print(i)
+					increased_days[i].total_free_days -= 1
+					increased_days[i].end_free_days_date = (test_date - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+				break
 
+			for index, service in enumerate(services):
+				free_days_old[index] = service.total_free_days
 			test_date += datetime.timedelta(days=1)
-		print(f"Total days: {self.service_a.days} - Free days {self.service_a.free_days}")
-		print(f"Total days: {self.service_b.days} - Free days {self.service_b.free_days}")
-		print(f"Total days: {self.service_c.days} - Free days {self.service_c.free_days}")
 
-	@property
-	def price(self):
-		# Find out which services the customer is paying for
+	def _calculate_discount_days(self, services):
+		for service in services:
+			if service.discount > 0:
+				max_date = max(max(service.discount_start, self.start_date),
+							   max(service.discount_start, service.end_free_days_date))
+				if service.discount_end is None:
+					service.discounted_days = functions.calculate_days_difference(max_date,
+																				  min(service.end_date, self.end_date)
+																				  , service.workday)
+				else:
+					service.discounted_days = functions.calculate_days_difference(max_date,
+																				  service.discount_end, service.workday)
+				if service.discounted_days < 0:
+					service.discounted_days = 0
+
+	def _determine_active_services(self):
 		active_services = [self.service_a, self.service_b, self.service_c]
 		to_remove = []
 
 		for service in active_services:
-			if service.start_date == "None":
+			if service.start_date is None:
 				to_remove.append(service)
-			else:
-				service.days = functions.calculate_days_difference(service.start_date, service.end_date,
-																   businessdays=service.workday)
 
 		for inactive_service in to_remove:
 			if inactive_service in active_services:
 				active_services.remove(inactive_service)
 
-		# Evaluate the number of free days to be removed for each service
-		if (self.service_a.days + self.service_b.days + self.service_c.days) < int(self.free_days):
+		return active_services
+
+	def _calculate_price(self, active_services):
+		price = 0
+		for service in active_services:
+			price += (service.discounted_days * (1 - service.discount / 100)
+					  + (service.active_days - service.discounted_days)) * service.price
+		return price
+
+	def calculate_price(self) -> float:
+		# Find out which services the customer is paying for
+		# and calculate the total number of days for each service
+		active_services = self._determine_active_services()
+		for service in active_services:
+			service.total_days = functions.calculate_days_difference(service.start_date, service.end_date,
+																	 businessdays=service.workday)
+
+		# if the collective number of days is lower than the number of free days => price is 0
+		if (self.service_a.total_days + self.service_b.total_days + self.service_c.total_days) < int(self.free_days):
 			self._price = 0
 			return self._price
 
+		# Evaluate the end date of free days for each service
 		if self.free_days != 0:
-			self._calculate_free_days(active_services)
+			self._calculate_free_days_end(active_services)
+
+		for service in active_services:
+			service.active_date = service.end_free_days_date
+			service.active_days = functions.calculate_days_difference(service.active_date, service.end_date,
+																	  businessdays=service.workday)
+			print(service.active_days)
 
 		# Evaluate the number of days with a discount applied for each service
-		self._price = 10000
+		self._calculate_discount_days(active_services)
+		for service in active_services:
+			print(f"Discounted days: {service.discounted_days} - "
+				  f"Total active days {service.active_days}")
+
+		self._price = round(self._calculate_price(active_services), 2)
 		return self._price
 
-		# Calculate the price for each service
-
-		# Calculate the final price
 
 class Service():
 	def __init__(self, price, start_date, end_date, discount, discount_start, discount_end, workday: bool):
 		self.price = price
 		self.start_date = start_date
+		self.active_date = start_date
 		self.end_date = end_date
 		self.discount = discount
 		self.discount_start = discount_start
 		self.discount_end = discount_end
 		self.workday = workday
-		self.days = 0
-		self.free_days = 0
+		self.total_days = 0
+		self.active_days = 0
+		self.total_free_days = 0
+		self.discounted_days = 0
+		self.end_free_days_date = start_date
 
 
 def main():
-	received_customerid = "Customer Y"
-	received_start_date = "2018-01-01"
-	received_end_date = "2019-10-01"
+	received_customerid = "Customer X"
+	received_start_date = "2018-09-20"
+	received_end_date = "2018-10-01"
+	# Check that end date is later than start date
 
 	# Fetch data from the DB
 	customer_data = {}
@@ -117,8 +182,8 @@ def main():
 						start_date=received_start_date,
 						end_date=received_end_date)
 
-	print(customer.price)
-
+	price = customer.calculate_price()
+	print(price)
 
 if __name__ == "__main__":
 	main()
